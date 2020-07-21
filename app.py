353e3672870ebe5c84b4d3b9cf47db9ea20edd26
codewouter <wpc_okhuijsen@hotmail.com>
2020-07-21 14:19:53 +0000

    Connect mongo database NPCLibrary and complete test succesfully. Set env.py and add to gitignore

--- app.py
+++ app.py
@@ -1,12 +1,22 @@
 import os
-from flask import Flask
+from flask import Flask, render_template, redirect, request, url_for
+from flask_pymongo import PyMongo
+from bson.objectid import ObjectId
+
+if os.path.exists("env.py"):
+    import env
 
 app = Flask(__name__)
 
+app.config["MONGO_DBNAME"] = 'task_manager'
+app.config["MONGO_URI"] = os.getenv('MONGO_URI', 'mongodb://localhost')
+
+mongo = PyMongo(app)
 
 @app.route('/')
-def hello():
-    return 'Hello World of non player characters'
+@app.route('/get_npcs')
+def get_npcs():
+    return render_template("npcs.html", npcs=mongo.db.NPC.find())
 
 
 if __name__ == '__main__':
