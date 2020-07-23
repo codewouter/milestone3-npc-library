b2bc10b8538a611513d2be37b29d8eaa52ba4da1
codewouter <wpc_okhuijsen@hotmail.com>
2020-07-23 12:43:34 +0000

    add npc_overview page; add view button
    Add view button to every entry in the NPC list. Add anchor to button
    so clicking will bring up the npc overview page of the selected
    npc

--- app.py
+++ app.py
@@ -8,7 +8,7 @@ if os.path.exists("env.py"):
 
 app = Flask(__name__)
 
-app.config["MONGO_DBNAME"] = 'task_manager'
+app.config["MONGO_DBNAME"] = 'NPCLibrary'
 app.config["MONGO_URI"] = os.getenv('MONGO_URI', 'mongodb://localhost')
 
 mongo = PyMongo(app)
@@ -25,8 +25,13 @@ def get_npcs():
     return render_template("npcs.html", npcs=mongo.db.NPC.find())
 
 
+@app.route('/npc_overview/<npc_id>')
+def npc_overview(npc_id):
+    currentNPC = mongo.db.NPC.find_one({"_id": ObjectId(npc_id)})
+    return render_template('npcoverview.html', npc=currentNPC)
+
+
 if __name__ == '__main__':
     app.run(host=os.environ.get('IP'),
             port=int(os.environ.get('PORT')),
             debug=True)
-            
