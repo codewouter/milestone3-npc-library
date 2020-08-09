c849b33522a6c5bd77da4d3a73917a718897c1c1
codewouter <wpc_okhuijsen@hotmail.com>
2020-08-09 13:38:23 +0000

    Set debug to false

--- app.py
+++ app.py
@@ -195,4 +195,4 @@ def delete_class(class_id):
 if __name__ == '__main__':
     app.run(host=os.environ.get('IP'),
             port=int(os.environ.get('PORT')),
-            debug=True)
+            debug=False)
