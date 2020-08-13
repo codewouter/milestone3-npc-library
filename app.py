07f70dd28fbe376626ab16002aa1de4e904d5f17
codewouter <wpc_okhuijsen@hotmail.com>
2020-08-13 08:40:28 +0000

    clean up app.py according to pep8 validator

--- app.py
+++ app.py
@@ -24,21 +24,25 @@ def welcome():
 
 
 # ---------- routes for NPCs ----------
-# This route/function gets all NPC entries from the database and passes them to render the NPC overview list
+# This route/function gets all NPC entries from the database and
+# passes them to render the NPC overview list
 @app.route('/get_npcs')
 def get_npcs():
     return render_template("npcs.html",
                            npcs=mongo.db.NPC.find().sort('name', 1))
 
 
-# This route/function gets all race and class entries from the db and passes them to render the newnpc page 
+# This route/function gets all race and class entries from the db
+# and passes them to render the newnpc page
 @app.route('/add_npc')
 def add_npc():
     allRaces = mongo.db.race.find().sort('race', 1)
     allClasses = mongo.db.NPCClass.find().sort('class', 1)
     return render_template('newnpc.html', races=allRaces, classes=allClasses)
 
-# This route/function will read the form on the newnpc.html page and insert it as a new record in the dabase. 
+
+# This route/function will read the form on the newnpc.html page
+# and insert it as a new record in the dabase.
 @app.route('/insert_npc', methods=['POST'])
 def insert_npc():
     npcs = mongo.db.NPC
@@ -56,15 +60,20 @@ def insert_npc():
     npcs.insert_one(newNPCObject)
     return redirect(url_for('get_npcs'))
 
-# This route/function will recieve the relevant npc that needs to be viewed by the user and get the relevant data from the database.
-# The data will then be passed to render the npcoverview.html page displaying all content about the specific NPC.
+
+# This route/function will recieve the relevant npc that needs
+# to be viewed by the user and get the relevant data from the database.
+# The data will then be passed to render the npcoverview.html page
+# displaying all content about the specific NPC.
 @app.route('/npc_overview/<npc_id>')
 def npc_overview(npc_id):
     currentNPC = mongo.db.NPC.find_one({"_id": ObjectId(npc_id)})
     return render_template('npcoverview.html', npc=currentNPC)
 
-# This route/function will get all necessary values about the specific NPC that is selected,
-# then pass all values to render the editnpc page so known values are prefilled in the edit form.
+
+# This route/function will get all necessary values about the specific
+# NPC that is selected, then pass all values to render the editnpc page
+# so known values are prefilled in the edit form.
 @app.route('/edit_npc/<npc_id>')
 def edit_npc(npc_id):
     currentNPC = mongo.db.NPC.find_one({"_id": ObjectId(npc_id)})
@@ -73,8 +82,10 @@ def edit_npc(npc_id):
     return render_template('editnpc.html', npc=currentNPC, races=allRaces,
                            classes=allClasses)
 
-# This route/function will take all data from the form on the editnpc.html page and combined with the relevant id that is being passed from the page,
-#  update the values in the database.
+
+# This route/function will take all data from the form on the editnpc.html page
+# and combined with the relevant id that is being passed from the page,
+# update the values in the database.
 @app.route('/update_npc/<npc_id>', methods=["POST"])
 def update_npc(npc_id):
     npcs = mongo.db.NPC
@@ -105,7 +116,8 @@ def update_npc(npc_id):
     return redirect(url_for('npc_overview', npc_id=npc_id))
 
 
-# This route/function will recive the selected npc_id and delete the entry from the database.
+# This route/function will recive the selected npc_id
+# and delete the entry from the database.
 @app.route('/delete_npc/<npc_id>')
 def delete_npc(npc_id):
     mongo.db.NPC.remove({'_id': ObjectId(npc_id)})
@@ -113,18 +125,22 @@ def delete_npc(npc_id):
 
 
 # ---------- routes for races ----------
-# This route/function gets all race entries from the database and passes them to render race list
+# This route/function gets all race entries from the database
+# and passes them to render race list
 @app.route('/get_races')
 def get_races():
     return render_template('races.html',
                            races=mongo.db.race.find().sort('race', 1))
+
 
 # This route/function will render the newrace.html page.
 @app.route('/add_race')
 def add_race():
     return render_template('newrace.html')
 
-# This route/function will read the form on the newrace.html page and insert it as a new record in the dabase
+
+# This route/function will read the form on the newrace.html page
+# and insert it as a new record in the dabase
 @app.route('/insert_race', methods=['POST'])
 def insert_race():
     races = mongo.db.race
@@ -133,15 +149,19 @@ def insert_race():
     races.insert_one(newRaceObject)
     return redirect(url_for('get_races'))
 
-# This route/function will get all necessary values about the specific race that is selected,
-# then pass all values to render the editrace page so known values are prefilled in the edit form.
+
+# This route/function will get all necessary values about the specific
+# race that is selected, then pass all values to render the editrace
+# page so known values are prefilled in the edit form.
 @app.route('/edit_race/<race_id>')
 def edit_race(race_id):
     currentRace = mongo.db.race.find_one({"_id": ObjectId(race_id)})
     return render_template('editrace.html', race=currentRace)
 
-# This route/function will take all data from the form on the editrace.html page and combined with the relevant id that is being passed from the page,
-#  then update the values in the database.
+
+# This route/function will take all data from the form on the
+# editrace.html page and combined with the relevant id that
+# is being passed from the page, then update the values in the database.
 @app.route('/update_race/<race_id>', methods=["POST"])
 def update_race(race_id):
     races = mongo.db.race
@@ -151,7 +171,9 @@ def update_race(race_id):
     })
     return redirect(url_for('get_races'))
 
-# This route/function will recive the selected race_id and delete the entry from the database.
+
+# This route/function will recive the selected race_id
+# and delete the entry from the database.
 @app.route('/delete_race/<race_id>')
 def delete_race(race_id):
     mongo.db.race.remove({'_id': ObjectId(race_id)})
@@ -159,11 +181,13 @@ def delete_race(race_id):
 
 
 # ---------- routes for classes ----------
-# This route/function gets all class entries from the database and passes them to render race list
+# This route/function gets all class entries from the database
+# and passes them to render race list
 @app.route('/get_classes')
 def get_classes():
     return render_template('classes.html',
                            classes=mongo.db.NPCClass.find().sort('class', 1))
+
 
 # This route/function will render the newclass.html page.
 @app.route('/add_class')
@@ -171,7 +195,8 @@ def add_class():
     return render_template('newclass.html')
 
 
-# This route/function will read the form on the newclass.html page and insert it as a new record in the dabase
+# This route/function will read the form on the newclass.html page
+# and insert it as a new record in the dabase
 @app.route('/insert_class', methods=['POST'])
 def insert_class():
     classes = mongo.db.NPCClass
@@ -180,15 +205,19 @@ def insert_class():
     classes.insert_one(newClassObject)
     return redirect(url_for('get_classes'))
 
-# This route/function will get all necessary values about the specific class that is selected,
-# then pass all values to render the editclass page so known values are prefilled in the edit form.
+
+# This route/function will get all necessary values about the
+# specific class that is selected, then pass all values to render
+# the editclass page so known values are prefilled in the edit form.
 @app.route('/edit_class/<class_id>')
 def edit_class(class_id):
     currentClass = mongo.db.NPCClass.find_one({"_id": ObjectId(class_id)})
     return render_template('editclass.html', NPCClass=currentClass)
 
-# This route/function will take all data from the form on the editclass.html page and combined with the relevant id that is being passed from the page,
-#  then update the values in the database.
+
+# This route/function will take all data from the form on the editclass.html
+# page and combined with the relevant id that is being passed from the page,
+# then update the values in the database.
 @app.route('/update_class/<class_id>', methods=["POST"])
 def update_class(class_id):
     classes = mongo.db.NPCClass
@@ -198,7 +227,9 @@ def update_class(class_id):
     })
     return redirect(url_for('get_classes'))
 
-# This route/function will recive the selected class_id and delete the entry from the database.
+
+# This route/function will recive the selected class_id
+# and delete the entry from the database.
 @app.route('/delete_class/<class_id>')
 def delete_class(class_id):
     mongo.db.NPCClass.remove({'_id': ObjectId(class_id)})
